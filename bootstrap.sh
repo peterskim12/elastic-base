dea349c5da20260a4d960502e05515fe0a02f3dc
Peter Kim <petedogg@gmail.com>
2016-12-16 21:11:48 -0500

    change to Ubuntu 16.04; add Sense; update to ES 2.4.3

--- bootstrap.sh
+++ bootstrap.sh
@@ -13,25 +13,25 @@ apt-get -y -q install oracle-java8-installer
 update-java-alternatives -s java-8-oracle
 
 # Download the Elastic product tarballs
-wget https://download.elastic.co/elasticsearch/release/org/elasticsearch/distribution/tar/elasticsearch/2.4.0/elasticsearch-2.4.0.tar.gz
-wget https://download.elastic.co/kibana/kibana/kibana-4.6.0-linux-x86_64.tar.gz
-wget https://download.elastic.co/logstash/logstash/logstash-2.4.0.tar.gz
-wget https://download.elastic.co/beats/topbeat/topbeat-1.3.0-x86_64.tar.gz
-wget https://download.elastic.co/beats/packetbeat/packetbeat-1.3.0-x86_64.tar.gz
-wget https://download.elastic.co/beats/filebeat/filebeat-1.3.0-x86_64.tar.gz
+wget https://download.elastic.co/elasticsearch/release/org/elasticsearch/distribution/tar/elasticsearch/2.4.3/elasticsearch-2.4.3.tar.gz
+wget https://download.elastic.co/kibana/kibana/kibana-4.6.3-linux-x86_64.tar.gz
+wget https://download.elastic.co/logstash/logstash/logstash-2.4.1.tar.gz
+wget https://artifacts.elastic.co/downloads/beats/metricbeat/metricbeat-5.1.1-linux-x86_64.tar.gz
+wget https://artifacts.elastic.co/downloads/beats/packetbeat/packetbeat-5.1.1-linux-x86_64.tar.gz
+wget https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-5.1.1-linux-x86_64.tar.gz
 
 # Untar the bits
 sudo -u vagrant bash -c 'for f in *.tar.gz; do tar xf $f; done'
 
 # Add required encryptionKey for Reporting
-cat <<KIBANA_CONF >> /opt/elastic/kibana-4.6.0-linux-x86_64/config/kibana.yml
+cat <<KIBANA_CONF >> /opt/elastic/kibana-4.6.3-linux-x86_64/config/kibana.yml
 reporting.encryptionKey: "FeelTheBern"
 KIBANA_CONF
 
 # Recommended ES settings
 # START BOOTSTRAP CHECKS CONFIG CHANGES #
-cat <<ES_CONF >> /opt/elastic/elasticsearch-2.4.0/config/elasticsearch.yml
-network.host: [_local_, _eth1_]
+cat <<ES_CONF >> /opt/elastic/elasticsearch-2.4.3/config/elasticsearch.yml
+network.host: [_local_, _enp0s8_]
 
 bootstrap.mlockall: true
 discovery.zen.minimum_master_nodes: 1
@@ -67,7 +67,7 @@ SECLIMITS
 # END BOOTSTRAP CHECKS CONFIG CHANGES #
 
 # Install license plugin
-cd /opt/elastic/elasticsearch-2.4.0
+cd /opt/elastic/elasticsearch-2.4.3
 sudo -u vagrant bash -c 'bin/plugin install elasticsearch/license/latest'
 
 # Install Watcher in Elasticsearch
@@ -81,10 +81,11 @@ sudo -u vagrant bash -c 'bin/plugin install graph'
 sudo -u vagrant nohup bash -c 'bin/elasticsearch' <&- &>/dev/null &
 
 # Install Marvel, Graph and Reporting in Kibana
-cd /opt/elastic/kibana-4.6.0-linux-x86_64
+cd /opt/elastic/kibana-4.6.3-linux-x86_64
 sudo -u vagrant bash -c 'bin/kibana plugin --install elasticsearch/marvel/latest'
 sudo -u vagrant bash -c 'bin/kibana plugin --install kibana/reporting/latest'
 sudo -u vagrant bash -c 'bin/kibana plugin --install elasticsearch/graph/latest'
+sudo -u vagrant bash -c 'bin/kibana plugin --install elastic/sense'
 
 # Run Kibana
 sudo -u vagrant nohup bash -c 'bin/kibana' <&- &>/dev/null &
