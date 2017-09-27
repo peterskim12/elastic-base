d23e523dab87a6146c3ad82dd5a4d699a4f10bff
Peter Kim <petedogg@gmail.com>
2017-09-26 23:18:59 -0400

    update for 6.0.0-beta2; add password generation

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,24 +10,24 @@ apt-get -y -q update
 apt-get -y -q install openjdk-8-jdk
 
 # Download the Elastic product tarballs
-wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-5.4.2.tar.gz
-wget https://artifacts.elastic.co/downloads/kibana/kibana-5.4.2-linux-x86_64.tar.gz
-wget https://artifacts.elastic.co/downloads/logstash/logstash-5.4.2.tar.gz
-wget https://artifacts.elastic.co/downloads/beats/metricbeat/metricbeat-5.4.2-linux-x86_64.tar.gz
-wget https://artifacts.elastic.co/downloads/beats/packetbeat/packetbeat-5.4.2-linux-x86_64.tar.gz
-wget https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-5.4.2-linux-x86_64.tar.gz
+wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-6.0.0-beta2.tar.gz
+wget https://artifacts.elastic.co/downloads/kibana/kibana-6.0.0-beta2-linux-x86_64.tar.gz
+wget https://artifacts.elastic.co/downloads/logstash/logstash-6.0.0-beta2.tar.gz
+wget https://artifacts.elastic.co/downloads/beats/metricbeat/metricbeat-6.0.0-beta2-linux-x86_64.tar.gz
+wget https://artifacts.elastic.co/downloads/beats/packetbeat/packetbeat-6.0.0-beta2-linux-x86_64.tar.gz
+wget https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-6.0.0-beta2-linux-x86_64.tar.gz
 
 # Untar the bits
 sudo -u vagrant bash -c 'for f in *.tar.gz; do tar xf $f; done'
 
 # Allow all requests to Kibana
-cat <<KIBANA_CONF >> /opt/elastic/kibana-5.4.2-linux-x86_64/config/kibana.yml
+cat <<KIBANA_CONF >> /opt/elastic/kibana-6.0.0-beta2-linux-x86_64/config/kibana.yml
 server.host: "0.0.0.0"
 KIBANA_CONF
 
 # Recommended ES settings to pass bootstrap checks
 # START BOOTSTRAP CHECKS CONFIG CHANGES #
-cat <<ES_CONF >> /opt/elastic/elasticsearch-5.4.2/config/elasticsearch.yml
+cat <<ES_CONF >> /opt/elastic/elasticsearch-6.0.0-beta2/config/elasticsearch.yml
 http.host: [_local_, _enp0s8_]
 path.repo: ["/vagrant/es_snapshots"]
 
@@ -35,16 +35,13 @@ bootstrap.memory_lock: true
 discovery.zen.minimum_master_nodes: 1
 ES_CONF
 
-sed -i -e 's/Xms2g/Xms1g/g' /opt/elastic/elasticsearch-5.4.2/config/jvm.options
-sed -i -e 's/Xmx2g/Xmx1g/g' /opt/elastic/elasticsearch-5.4.2/config/jvm.options
-
 sysctl -w vm.max_map_count=262144
 cat <<SYSCTL >> /etc/sysctl.conf
 vm.max_map_count=262144
 SYSCTL
 
 ulimit -n 65536
-ulimit -u 2048
+ulimit -u 4096
 ulimit -l unlimited
 cat <<SECLIMITS >> /etc/security/limits.conf
 *                soft    nofile         1024000
@@ -55,8 +52,8 @@ vagrant           soft    nofile         1024000
 vagrant           hard    nofile         1024000
 vagrant           soft    memlock        unlimited
 vagrant           hard    memlock        unlimited
-vagrant           soft    nproc        2048
-vagrant           hard    nproc        2048
+vagrant           soft    nproc        4096
+vagrant           hard    nproc        4096
 vagrant           soft    as        unlimited
 vagrant           hard    as        unlimited
 root             soft    nofile         1024000
@@ -67,14 +64,55 @@ root           hard    as        unlimited
 SECLIMITS
 # END BOOTSTRAP CHECKS CONFIG CHANGES #
 
+cat <<LS_CONF >> /opt/elastic/logstash-6.0.0-beta2/config/logstash.yml
+xpack.monitoring.enabled: true
+xpack.management.enabled: true
+xpack.management.elasticsearch.url: "http://localhost:9200/"
+xpack.management.logstash.poll_interval: 5s
+xpack.management.pipeline.id: ["apache"]
+LS_CONF
+
 # Install X-Pack in Elasticsearch
-cd /opt/elastic/elasticsearch-5.4.2
+cd /opt/elastic/elasticsearch-6.0.0-beta2
+sudo -u vagrant bash -c 'bin/elasticsearch-plugin install repository-s3 --batch'
 sudo -u vagrant bash -c 'bin/elasticsearch-plugin install x-pack --batch'
 # Run Elasticsearch
 # sudo -u vagrant nohup bash -c 'bin/elasticsearch' <&- &>/dev/null &
 
 # Install X-Pack in Kibana
-cd /opt/elastic/kibana-5.4.2-linux-x86_64
+cd /opt/elastic/kibana-6.0.0-beta2-linux-x86_64
 sudo -u vagrant bash -c 'bin/kibana-plugin install x-pack'
 # Run Kibana
 # sudo -u vagrant nohup bash -c 'bin/kibana' <&- &>/dev/null &
+
+# Install X-Pack in Logstash
+cd /opt/elastic/logstash-6.0.0-beta2
+sudo -u vagrant bash -c 'bin/logstash-plugin install x-pack'
+
+# Run password setup
+# Run Elasticsearch
+cd /opt/elastic/elasticsearch-6.0.0-beta2
+sudo -u vagrant nohup bash -c 'bin/elasticsearch' <&- &>/dev/null &
+
+# Wait for Elasticsearch to startup
+sleep 30
+
+# Generate system user passwords
+sudo -u vagrant bash -c 'bin/x-pack/setup-passwords auto --batch > passwords.txt'
+
+# Write generated passwords to config files
+elasticpwd=$(sed -n 's/PASSWORD elastic = \(.\+\)/\1/p' passwords.txt)
+kibanapwd=$(sed -n 's/PASSWORD kibana = \(.\+\)/\1/p' passwords.txt)
+logstashpwd=$(sed -n 's/PASSWORD logstash_system = \(.\+\)/\1/p' passwords.txt)
+
+cat <<KIBANA_CONF >> /opt/elastic/kibana-6.0.0-beta2-linux-x86_64/config/kibana.yml
+elasticsearch.username: kibana
+elasticsearch.password: $kibanapwd
+KIBANA_CONF
+
+cat <<LS_CONF >> /opt/elastic/logstash-6.0.0-beta2/config/logstash.yml
+xpack.monitoring.elasticsearch.username: logstash_system
+xpack.monitoring.elasticsearch.password: $logstashpwd
+xpack.management.elasticsearch.username: elastic
+xpack.management.elasticsearch.password: $elasticpwd
+LS_CONF
